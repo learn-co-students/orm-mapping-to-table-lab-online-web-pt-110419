86f8b3ee7ed21c64a0e953456fe3d22eb28b36db
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-02-02 22:40:19 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,5 +1,18 @@
 class Student
+  attr_reader :id
+  attr_accessor :name, :grade
+  
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+  end
 
+def self.create_table(name, grade)
+  sql <<-SQL
+  
+  
+  SQL
+end
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
